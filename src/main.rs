109eb427758f725ecb0d4a5c23b6e0149f8389ae
xanderjakeq <xanderjakeq@pop-os.localdomain>
2023-02-05 15:45:30 -0500

    use PORT env

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,21 @@
 use std::net::TcpListener;
+use dotenv::dotenv;
+use std::env;
 
 use rust_server::run;
 
 #[tokio::main]
 async fn main () -> std::io::Result<()> {
-    let listener = TcpListener::bind("http://127.0.0.1:0").expect("Failed to bind random port");
+    dotenv().ok();
+    let listener = TcpListener::bind(format!(
+        "0.0.0.0:{}",
+        //"127.0.0.1:{}",
+        env::var("PORT").unwrap_or("0".to_owned())
+    ))
+    .expect("Failed to bind random port");
+    let port = listener.local_addr().unwrap().port();
+
+    println!("listening in port: {}", port);
+
     run(listener)?.await
 }
